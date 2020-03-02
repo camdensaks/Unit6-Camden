1396616a7286c3d020f294382d37e2d9b08f64f6
camdensaks <54951342+camdensaks@users.noreply.github.com>
2020-03-02 08:43:49 -0800

    Commit

---
+++ src/Deck.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Deck {
+    private ArrayList<Card> deck;
+
+    public Deck(){
+        deck = new  ArrayList<>();
+        for(String rank : Card.RANK){
+            for(String suit : Card.SUIT){
+                deck.add(new Card(rank, suit));
+            }
+        }
+    }
+public ArrayList<Card> getDeck(){
+        return deck;
+}
+
+public static void main(String[] args){
+        Deck d = new Deck();
+        System.out.println(d.getDeck());
+}
+}
